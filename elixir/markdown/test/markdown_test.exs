c950fccbf92405f6facaf9686caa37e53aab4930
felix <felixpowerbusiness@gmail.com>
2024-10-12 01:06:08 +0100

    Added multiline test

--- elixir/markdown/test/markdown_test.exs
+++ elixir/markdown/test/markdown_test.exs
@@ -20,6 +20,42 @@ defmodule MarkdownTest do
     end)
   end
 
+  test "Works for multiple headers in string" do
+    markdown = """
+    # hello
+    ## hello
+    ### hello
+    #### hello
+    ##### hello
+    ###### hello
+    """
+
+    html = """
+    <h1>hello</h1>
+    <h2>hello</h2>
+    <h3>hello</h3>
+    <h4>hello</h4>
+    <h5>hello</h5>
+    <h6>hello</h6>
+    """
+
+    assert Markdown.convert_to_html(markdown) == html
+  end
+
+  test "Works for header and normal text" do
+    markdown = """
+    # hello
+    world
+    """
+
+    html = """
+    <h1>hello</h1>
+    world
+    """
+
+    assert Markdown.convert_to_html(markdown) == html
+  end
+
   test "Expect dodgy data to be unchanged" do
     assert Markdown.convert_to_html("##-- hello") == "##-- hello"
   end
